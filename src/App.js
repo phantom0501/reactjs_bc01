123c152f37482043f3685d635d8ea2122d94d62e
phantom0501 <letrungcuong2k@gmail.com>
2021-03-01 18:42:01 +0700

    Install redux, react-redux library and Demo CartRedux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import LayoutExercises from "./components/layoutExercises/LayoutExercises";
 import ProductManagement from "./Props/ProductManagement/ProductManagement";
+import BTGioHangRedux from "./DemoRedux/BTGioHangRedux/BTGioHangRedux";
 
 function App() {
   return (
     <div className="App">
       {/* <LayoutExercises /> */}
-      <ProductManagement />
+      {/* <ProductManagement /> */}
+      <BTGioHangRedux />
     </div>
   );
 }
---
+++ src/DemoRedux/BTGioHangRedux/GioHangRedux.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+//Kết nối react component với redux store
+import { connect } from "react-redux";
+
+class GioHangRedux extends Component {
+  render() {
+    console.log(this.props.gioHang, "gioHang");
+    return (
+      <div className="container">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Mã SP</th>
+              <th>Tên SP</th>
+              <th>Số lượng</th>
+              <th>Giá</th>
+              <th>Thành tiền</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.gioHang.map((spGioHang, index) => {
+              return (
+                <tr key={index}>
+                  <td>{spGioHang.maSP}</td>
+                  <td>{spGioHang.tenSP}</td>
+                  <td>{spGioHang.soLuong}</td>
+                  <td>{spGioHang.gia}</td>
+                  <td>{spGioHang.gia * spGioHang.soLuong}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+//Hàm chuyển state trên redux trở thành props của component
+//state đại diện cho rootReducer
+const mapStateToProps = (state) => {
+  //Tạo props từ state redux
+  return {
+    gioHang: state.gioHangReducer.gioHang,
+  };
+};
+
+//Kết nối giữa component và redux
+export default connect(mapStateToProps)(GioHangRedux);
