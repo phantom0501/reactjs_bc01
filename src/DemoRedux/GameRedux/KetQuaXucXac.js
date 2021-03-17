fe2b8b2051eae16c2eddc22d2b545af46c6eef4a
phantom0501 <letrungcuong2k@gmail.com>
2021-03-17 15:50:02 +0700

    add all files edited

--- src/DemoRedux/GameRedux/KetQuaXucXac.js
+++ src/DemoRedux/GameRedux/KetQuaXucXac.js
@@ -1,19 +1,73 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
-export default class KetQuaXucXac extends Component {
+class KetQuaXucXac extends Component {
   render() {
+    let { banChon, soBanThang, tongSoBanChoi } = this.props;
     return (
       <div className="container text-center">
         <div className="display-4">
-          Bạn chọn: <span className="text-success">Tài</span>
+          Bạn chọn:
+          <span className="text-success"> {banChon}</span>
+        </div>
+        <div className="display-4 py-2">
+          Số bàn thắng: <span className="text-success">{soBanThang}</span>
         </div>
         <div className="display-4">
-          Số bàn thắng: <span className="text-success">5</span>
+          Số bàn chơi: <span className="text-primary">{tongSoBanChoi}</span>
         </div>
-        <div className="display-4">
-          Số bàn chơi: <span className="text-success">10</span>
-        </div>
+        <button
+          onClick={() => this.props.playGame()}
+          // onClick={() => {
+          //   const action = {
+          //     type: "PLAY_GAME",
+          //   };
+          //   this.props.dispatch(action);
+          // }}
+          className="btn btn-success"
+        >
+          Play game
+        </button>
       </div>
     );
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    banChon: state.gameXucXacReducers.banChon,
+    soBanThang: state.gameXucXacReducers.soBanThang,
+    tongSoBanChoi: state.gameXucXacReducers.tongSoBanChoi,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    playGame: () => {
+      let count = 0;
+      let randomXucXac = setInterval(() => {
+        dispatch({
+          type: "PLAY_GAME",
+        });
+
+        count++;
+        if (count > 10) {
+          clearInterval(randomXucXac);
+          dispatch({
+            type: "END_GAME",
+          });
+        }
+      }, 100);
+
+      // dispatch({
+      //   type: "PLAY_GAME",
+      // });
+
+      // dispatch({
+      //   type: "END_GAME",
+      // });
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(KetQuaXucXac);
